621e048719e6fb2f179be8cf9e9fea1681082cac
Nayla Nur Amalia <nayla.amalia@sigma.co.id>
2025-09-16 20:45:42 +0700

    chore(praktikum 2): updating conditions program

--- praktikum2-1/ParkirKendaraan.java
+++ praktikum2-1/ParkirKendaraan.java
@@ -5,31 +5,35 @@ public class ParkirKendaraan {
         Scanner input = new Scanner(System.in);
 
         int menit = input.nextInt();
-        String jenisKendaraan = input.next().toLowerCase(); 
+        String jenisKendaraan = input.next().toLowerCase();
         double biaya;
 
         double jam = menit / 60.0;
 
-        if (menit <= 0){
+        if (menit <= 0) {
             System.out.println("Misiiii input ga valid nihh!!");
             return;
-        } if (menit <= 5) {
+        }
+        if (menit <= 5) {
             biaya = 3000;
             System.out.printf("Biaya parkir %s hanya Rp %,.2f karena hanya %d menit", jenisKendaraan, biaya, menit);
             return;
         } else if (jenisKendaraan.equals("motor") || jenisKendaraan.equals("mobil")) {
             biaya = 5000;
+
             if (menit > 120) {
                 double extraMenit = menit - 120;
-                int biayaTambahan = (int) Math.ceil(extraMenit / 10.0); 
- 
+                int biayaTambahan = (int) Math.ceil(extraMenit / 10.0);
+
                 if (jenisKendaraan.equals("motor")) {
-                    biaya += biayaTambahan * 1000; 
-                } else { 
+                    biaya += biayaTambahan * 1000;
+                } else {
                     biaya += biayaTambahan * 2000;
                 }
+                System.out.printf("Biaya parkir %s selama %.1f jam adalah Rp%,.2f%n", jenisKendaraan, jam, biaya);
+            } else {
+                System.out.printf("Biaya parkir %s selama %.1f jam adalah Rp %,.2f%n", jenisKendaraan, jam, biaya);
             }
-            System.out.printf("Biaya parkir %s selama %.1f jam adalah Rp %, .2f%n", jenisKendaraan, jam, biaya);
             return;
         } else {
             System.out.println("Jalan kaki tidak perlu bayar parkir");
